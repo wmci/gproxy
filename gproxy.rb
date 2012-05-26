426a8f51ad0de5df4b9057c5c7a5b11bae81f837
Daniel Farrell <dfarrell@wmci.com>
2012-05-26 15:21:48 -0400

    Set the kerberos service from an env var

--- gproxy.rb
+++ gproxy.rb
@@ -5,7 +5,7 @@ require './lib/goliath/rack/reverse_proxy'
 
 class Gproxy < Goliath::API
   use Rack::Session::Cookie, key: 'gproxy.session'
-  use Goliath::Rack::Auth::Krb::BasicAndNego, ENV['REALM'], ENV['KEYTAB']
+  use Goliath::Rack::Auth::Krb::BasicAndNego, ENV['REALM'], ENV['KEYTAB'], ENV['SERVICE']
   use Goliath::Rack::Params
   use Goliath::Rack::ReverseProxy, base_url: ENV['BASE_URL']
 
